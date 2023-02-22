9b1f9c6bf29d70f5b545db2b45af16957b783b33
Pedro Fornari <pedro@fornari.org>
2023-02-22 10:08:27 -0300

    Entrega 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// import logo from './logo.svg';
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './Componentes/NavBar'
 import ItemListContainer from './Container/ItemListContainer';
